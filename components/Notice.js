c2efa201cb10393bb0525d83367b1e6f9ac78917
dguardado104 <dennis@turnkeyapparel.com>
2022-05-26 12:56:38 -0600

    add icons heroicons

--- components/Notice.js
+++ components/Notice.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { XIcon } from '@heroicons/react/solid'
 
 const Notice = () => {
 
@@ -15,7 +16,9 @@ const Notice = () => {
       <div className="bg-slate-300 rounded mb-4 p-4">
         <div className="flex justify-between items-center mb-4">
           <span className="font-bold">Lorem ipsum dolor</span>
-          <button className="btn-close-notice" onClick={handleCloseBtn}>Close</button>
+          <button className="btn-close-notice" onClick={handleCloseBtn}>
+            <XIcon className="h-5 w-5" />
+          </button>
         </div>
         <div>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptate sequi distinctio natus, ut nihil! Fugit expedita, sit distinctio quae ex ab praesentium laborum perspiciatis nihil voluptates, quis officia dolores!</p>
